a93f697c7a1c3d6871efd7bf58de762af1461a8a
Evan Brown <ebrown@signifyhealth.com>
2023-12-01 09:34:14 -0600

    get_input updates

---
+++ get_input.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+import argparse
+import subprocess
+import sys
+import requests
+
+# Usage: ./get_input.py > 1.in
+# You must fill in SESSION following the instructions below.
+# DO NOT run this in a loop, just once.
+
+# You can find SESSION by using Chrome tools:
+# 1) Go to https://adventofcode.com/2022/day/1/input
+# 2) right-click -> inspect -> click "Network".
+# 3) Refresh
+# 4) Click click
+# 5) Click cookies
+# 6) Grab the value for session. Fill it in.
+SESSION = '53616c7465645f5f0a46feb3b43909e35e47644548f5db0cfad3534d748b7b250e6a679982c639c64d97a76d31b583ba55a3da4e0b0f182dba881f8bb64829de'
+
+useragent = 'https://github.com/the-ebrown/AdventOfCode/blob/main/get_input.py by evan.m.brown@gmail.com'
+parser = argparse.ArgumentParser(description='Read input')
+parser.add_argument('--year', type=int, default=2023)
+parser.add_argument('--day', type=int, default=1)
+args = parser.parse_args()
+
+cmd = f'curl https://adventofcode.com/{args.year}/day/{args.day}/input --cookie "session={SESSION}" -A {useragent}'
+output = subprocess.check_output(cmd, shell=True)
+output = output.decode('utf-8')
+print(output, end='')
+print('\n'.join(output.split('\n')[:10]), file=sys.stderr)
